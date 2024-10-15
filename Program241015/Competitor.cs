f22b7e7bef479640ccceea425d81ace2b611f564
Ny19BalgaS <Ny19BalgaS@202-06>
2024-10-15 10:26:40 +0200

    Add project files.

---
+++ Program241015/Competitor.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program241015
+{
+    internal class Competitor
+    {
+        public string Name {  get; set; }
+        public int YearOfBirt {  get; set; }
+        public int RaceNumber { get; set; }
+        public bool Gender {  get; set; }
+        public string AgeCategory {  get; set; }
+        public Dictionary<string, TimeSpan> RaceTimes { get; set; }
+            
+        public Competitor(string row) 
+        {
+            var v = row.Split(';');
+            Name = v[0];
+            YearOfBirt = int.Parse(v[1]);
+            RaceNumber = int.Parse(v[2]);
+            Gender = v[3] == "f";
+            AgeCategory = v[4];
+            RaceTimes = new()
+            {
+                {"swimming",    TimeSpan.Parse(v[5])},
+                {"1st depot",   TimeSpan.Parse(v[6])},
+                {"cycling",     TimeSpan.Parse(v[7])},
+                {"2nd depot",   TimeSpan.Parse(v[8])},
+                {"running",     TimeSpan.Parse(v[9])},
+            };
+
+        }
+    }
+}
---
+++ Program241015/Program.cs
@@ -0,0 +1,9 @@
+﻿using Program241015;
+
+List<Competitor> competitors = [];
+
+using StreamReader sr = new("..\\..\\..\\src\\forras.txt", System.Text.Encoding.UTF8);
+while (!sr.EndOfStream) competitors.Add(new(sr.ReadLine()));
+Console.WriteLine($"Célbe érkező versenyzők száma: {competitors.Count}");
+
+//Győztes női versenyző
